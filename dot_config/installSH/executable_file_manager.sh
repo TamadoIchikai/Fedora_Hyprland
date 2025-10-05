4f7a388fcabddfd202814d9db2f14c4862be8c0f
TamadoIchikai <130322347+TamadoIchikai@users.noreply.github.com>
2025-10-05 23:24:12 +0700

    Update executable_file_manager.sh

--- dot_config/installSH/executable_file_manager.sh
+++ dot_config/installSH/executable_file_manager.sh
@@ -5,6 +5,10 @@ NC='\033[0m'
 
 set -e
 echo -e "${BLUE}-------> Installing Thunar file manager${NC}"
-sudo dnf install -y thunar thunar-archive-plugin thunar-volman gsettings-desktop-schemas
+sudo dnf install -y thunar thunar-archive-plugin thunar-volman gsettings-desktop-schemas gnome-themes-extra
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+gsettings set org.gnome.desktop.interface cursor-theme 'Adwaita'
 echo -e "${GREEN}-------> DONE${NC}"
 
