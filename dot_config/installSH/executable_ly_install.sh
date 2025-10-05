15684732a7c398be64967bf19fa97c6ec41515a6
TamadoIchikai <130322347+TamadoIchikai@users.noreply.github.com>
2025-10-05 23:27:06 +0700

    Update executable_ly_install.sh

--- dot_config/installSH/executable_ly_install.sh
+++ dot_config/installSH/executable_ly_install.sh
@@ -4,14 +4,14 @@ GREEN='\033[0;32m'
 NC='\033[0m' 
 
 echo -e "${BLUE}-------> install zig for ly ${NC}"
-sudo dnf install kernel-devel pam-devel libxcb-devel xorg-x1-xauth xorg-x11-server-Xorg brightnessctl
 cd ~/Downloads/Systems/
+sudo dnf install kernel-devel pam-devel libxcb-devel xorg-x11-xauth xorg-x11-server-Xorg brightnessctl
 wget https://ziglang.org/download/0.15.1/zig-x86_64-linux-0.15.1.tar.xz
 tar -xf Downloads/Systems/zig-x86_64-linux-0.15.1.tar.xz
 mv zig-x86_64-linux-0.15.1/ zig
 rm zig-x86_64-linux-0.15.1.tar.xz
 sudo ln -s ~/Downloads/Systems/zig/zig /usr/local/bin/zig
-echo "Zig version: $(zig -v)"
+echo "Zig version: $(zig version)"
 echo -e "${GREEN}-------> DONE${NC}"
 
 echo -e "${BLUE}-------> begin download ly and build the project ${NC}"
