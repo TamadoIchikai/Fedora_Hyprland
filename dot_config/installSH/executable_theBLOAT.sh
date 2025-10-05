6a07652bc59c5955f12785c2bf5d64f07567c947
TamadoIchikai <130322347+TamadoIchikai@users.noreply.github.com>
2025-10-05 23:28:27 +0700

    remove zoxide installation using rust (idk why i install it this way)

--- dot_config/installSH/executable_theBLOAT.sh
+++ dot_config/installSH/executable_theBLOAT.sh
@@ -5,18 +5,13 @@ NC='\033[0m'
 
 set -e
 echo -e "${BLUE}-------> Install some bloatwares lmao${NC}"
-sudo dnf install foot fuzzel fzf zsh firefox
+sudo dnf install foot fuzzel fzf zsh firefox zoxide
 
 echo "${BLUE}------->install flatpak related apps like obsidian, mission center${NC}"
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub io.missioncenter.MissionCenter
 flatpak override --user --socket=system-bus org.missioncenter.MissionCenter
 flatpak install flathub it.mijorus.gearlever
-
-echo "${BLUE}-------> install minimal cargo for some app like zoxide${NC}"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --profile=minimal
-source ~/.bashrc
-cargo install zoxide --locked
 
 echo "${BLUE}------->Building rofi for wayland support${NC}"
 sudo dnf install cairo-devel pango-devel glib2-devel libxkbcommon-devel wayland-devel wayland-protocols-devel pkg-config meson cmake rofi-devel qalc
