7a5a082052c0bcff119171416edde656e54ff447
TamadoIchikai <130322347+TamadoIchikai@users.noreply.github.com>
2025-10-05 23:28:53 +0700

    missing copr (smh)

--- dot_config/installSH/executable_waybar.sh
+++ dot_config/installSH/executable_waybar.sh
@@ -5,7 +5,7 @@ NC='\033[0m'
 
 set -e
 echo -e "${BLUE}-------> install waybar and some related modules ${NC}"
-sudo dnf enable erikreider/SwayNotificationCenter
+sudo dnf copr enable erikreider/SwayNotificationCenter
 sudo dnf install waybar kde-connect blueman pavucontrol zenity SwayNotificationCenter mpv mpv-mpris playerctl
 echo -e "${GREEN}-------> DONE ${NC}"
 
