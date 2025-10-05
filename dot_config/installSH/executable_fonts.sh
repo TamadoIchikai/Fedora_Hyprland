bfd2ef14261bf639d9b8a82447581e574d47f69e
TamadoIchikai <130322347+TamadoIchikai@users.noreply.github.com>
2025-10-05 23:26:01 +0700

    Update executable_fonts.sh

--- dot_config/installSH/executable_fonts.sh
+++ dot_config/installSH/executable_fonts.sh
@@ -5,9 +5,9 @@ NC='\033[0m'
 
 set -e
 echo -e "${BLUE}-------> Install fonts and icon${NC}"
-sudo dnf install fontawesome-fonts
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts
+sudo dnf install fontawesome-fonts
 wget -O JetBrainsMono.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip
 wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/NerdFontsSymbolsOnly.zip
 unzip NerdFontsSymbolsOnly.zip
