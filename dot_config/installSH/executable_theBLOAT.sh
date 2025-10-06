be66988b5683d3d6adb79a1330fc141739776ad2
TamadoIchikai <130322347+TamadoIchikai@users.noreply.github.com>
2025-10-06 10:07:37 +0700

    Update executable_theBLOAT.sh

--- dot_config/installSH/executable_theBLOAT.sh
+++ dot_config/installSH/executable_theBLOAT.sh
@@ -9,9 +9,7 @@ sudo dnf install foot fuzzel fzf zsh firefox zoxide
 
 echo "${BLUE}------->install flatpak related apps like obsidian, mission center${NC}"
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub io.missioncenter.MissionCenter
-flatpak override --user --socket=system-bus org.missioncenter.MissionCenter
-flatpak install flathub it.mijorus.gearlever
+
 
 echo "${BLUE}------->Building rofi for wayland support${NC}"
 sudo dnf install cairo-devel pango-devel glib2-devel libxkbcommon-devel wayland-devel wayland-protocols-devel pkg-config meson cmake rofi-devel qalc
