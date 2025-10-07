a14eaefbc226f71cd2174b2107410778983fffc9
TamadoIchikai <tamado.ichikai.kyooiku@gmail.com>
2025-10-07 21:43:21 +0700

    kdeconnect working now eyyyyy

--- dot_config/installSH/executable_waybar.sh
+++ dot_config/installSH/executable_waybar.sh
@@ -6,6 +6,13 @@ NC='\033[0m'
 set -e
 echo -e "${BLUE}-------> install waybar and some related modules ${NC}"
 sudo dnf copr enable erikreider/SwayNotificationCenter
-sudo dnf install -y waybar kde-connect blueman pavucontrol zenity SwayNotificationCenter mpv mpv-mpris playerctl
+sudo dnf install -y waybar kde-connect blueman pavucontrol zenity SwayNotificationCenter mpv mpv-mpris playerctl ufw
+sudo ufw enable
+sudo firewall-cmd --permanent --add-port=1714-1764/tcp
+sudo firewall-cmd --permanent --add-port=1714-1764/udp
+sudo firewall-cmd --reload
+sudo firewall-cmd --list-ports
+sudo ufw allow 1714:1764/udp comment "Allow KDE Connect UDP"
+sudo ufw allow 1714:1764/tcp comment "Allow KDE Connect TCP"
 echo -e "${GREEN}-------> DONE ${NC}"
 
