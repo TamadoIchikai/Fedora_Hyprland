557c79b0b4f5ceb8e2b547bb7446a41c367a5cdd
TamadoIchikai <tamado.ichikai.kyooiku@gmail.com>
2025-10-08 18:41:18 +0700

    make the font much bigger than before

--- dot_local/bin/executable_qalc_floating.sh
+++ dot_local/bin/executable_qalc_floating.sh
@@ -1,21 +1,24 @@
-
 #!/usr/bin/env bash
-# Floating centered Qalculate popup on Hyprland
+# Floating centered Qalculate popup on Hyprland with larger font
 
-TERMINAL="foot"                # change to alacritty, kitty, etc. if you like
-WINDOW_TITLE="QalcPopup"       # used for window rules
-GEOM="60% 50%"                 # width x height ratio
+TERMINAL="foot"
+WINDOW_TITLE="QalcPopup"
+GEOM="40% 50%"
+FONT_SIZE="13"  # change this to adjust size
 
-# Detect if already running, to make it togglable
+# Toggle behavior: close if already running
 if pgrep -f "qalc.*--title $WINDOW_TITLE" >/dev/null; then
     pkill -f "qalc.*--title $WINDOW_TITLE"
     exit 0
 fi
 
-# Launch floating centered terminal running qalc
-hyprctl dispatch exec "[float;center;size $GEOM;title $WINDOW_TITLE] $TERMINAL --title $WINDOW_TITLE bash -c '
-clear; 
-echo \"Qalculate ready.\"; 
+# Launch with larger font override
+hyprctl dispatch exec "[float;center;size $GEOM;title $WINDOW_TITLE] \
+$TERMINAL --title $WINDOW_TITLE \
+--override font='JetBrainsMono Nerd Font:size=$FONT_SIZE' \
+bash -c '
+clear;
+echo \"Qalculate ready.\";
 qalc
 '"
 
