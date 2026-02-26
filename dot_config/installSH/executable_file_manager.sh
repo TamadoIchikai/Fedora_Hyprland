4a02ccd78e9fc26f48e30544ae66e56747107aff
TamadoIchikai <tamado.ichikai.kyooiku@gmail.com>
2026-02-26 22:48:10 +0700

    change thunar to nautilus, add core utils and change mpv shortcut, appearance

--- dot_config/installSH/executable_file_manager.sh
+++ dot_config/installSH/executable_file_manager.sh
@@ -5,11 +5,45 @@ NC='\033[0m'
 
 set -e
 echo -e "${BLUE}-------> Installing Thunar file manager${NC}"
-sudo dnf install -y thunar thunar-archive-plugin thunar-volman xarchiver gsettings-desktop-schemas gnome-themes-extra papirus-icon-theme
+sudo dnf install -y nautilus dconf gvfs gvfs-fuse xarchiver gsettings-desktop-schemas gnome-themes-extra papirus-icon-theme shared-mime-info xdg-utils evince-thumbnailer ffmpegthumbnailer desktop-file-utils
 sleep 1
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 gsettings set org.gnome.desktop.interface cursor-theme 'Adwaita'
+sleep 1
+
+echo "${BLUE}-------> Set default application${NC}"
+xdg-mime default swayimg.desktop image/jpeg
+xdg-mime default swayimg.desktop image/png
+xdg-mime default swayimg.desktop image/gif
+xdg-mime default swayimg.desktop image/webp
+xdg-mime default swayimg.desktop image/svg+xml
+xdg-mime default swayimg.desktop image/avif
+xdg-mime default swayimg.desktop image/avifs
+xdg-mime default vlc.desktop video/mpeg
+xdg-mime default vlc.desktop video/ogg
+xdg-mime default vlc.desktop video/webm
+xdg-mime default vlc.desktop video/mp4
+xdg-mime default vlc.desktop audio/mpeg        
+xdg-mime default vlc.desktop audio/mp4         
+xdg-mime default vlc.desktop audio/ogg
+xdg-mime default code.desktop text/plain
+xdg-mime default code.desktop text/x-python
+xdg-mime default code.desktop text/x-c
+xdg-mime default code.desktop text/x-c++src
+xdg-mime default code.desktop text/html
+xdg-mime default code.desktop text/css
+xdg-mime default code.desktop application/javascript
+xdg-mime default code.desktop application/json
+xdg-mime default code.desktop text/markdown
+xdg-mime default code.desktop application/x-shellscript
+xdg-mime default code.desktop text/yaml
+xdg-mime default code.desktop application/xml
+xdg-mime default nautilus.desktop inode/directory
+
+sudo update-desktop-database
+sudo update-mime-database /usr/share/mime
+
 echo -e "${GREEN}-------> DONE${NC}"
 
--- dot_config/installSH/executable_theBLOAT.sh
+++ dot_config/installSH/executable_theBLOAT.sh
@@ -6,40 +6,12 @@ NC='\033[0m'
 set -e
 echo -e "${BLUE}-------> Install some bloatwares lmao${NC}"
 sudo dnf copr enable dejan/lazygit
-sudo dnf install foot fuzzel fzf firefox zoxide cliphist fuse fuse-libs qalculate setxkbmap trash-cli swappy texlive-scheme-full btop lazygit duf gdu
+sudo dnf install foot fuzzel fzf zoxide cliphist fuse fuse-libs qalculate setxkbmap trash-cli swappy texlive-scheme-full btop lazygit duf gdu
 
 echo -e "${BLUE}------->install flatpak related apps like obsidian, mission center${NC}"
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub md.obsidian.Obsidian
 flatpak install flathub org.zotero.Zotero
 
-echo "${BLUE}-------> Set default application${NC}"
-xdg-mime default swayimg.desktop image/jpeg
-xdg-mime default swayimg.desktop image/png
-xdg-mime default swayimg.desktop image/gif
-xdg-mime default swayimg.desktop image/webp
-xdg-mime default swayimg.desktop image/svg+xml
-xdg-mime default swayimg.desktop image/avif
-xdg-mime default swayimg.desktop image/avifs
-xdg-mime default vlc.desktop video/mpeg
-xdg-mime default vlc.desktop video/ogg
-xdg-mime default vlc.desktop video/webm
-xdg-mime default vlc.desktop video/mp4
-xdg-mime default vlc.desktop audio/mpeg        
-xdg-mime default vlc.desktop audio/mp4         
-xdg-mime default vlc.desktop audio/ogg
-xdg-mime default code.desktop text/plain
-xdg-mime default code.desktop text/x-python
-xdg-mime default code.desktop text/x-c
-xdg-mime default code.desktop text/x-c++src
-xdg-mime default code.desktop text/html
-xdg-mime default code.desktop text/css
-xdg-mime default code.desktop application/javascript
-xdg-mime default code.desktop application/json
-xdg-mime default code.desktop text/markdown
-xdg-mime default code.desktop application/x-shellscript
-xdg-mime default code.desktop text/yaml
-xdg-mime default code.desktop application/xml
-xdg-mime default thunar.desktop inode/directory
-
 echo -e "${GREEN}-------> DONE${NC}"
+
