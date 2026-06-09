da09bbdf563535575823a08965680ba85836ec02
TamadoIchikai <130322347+TamadoIchikai@users.noreply.github.com>
2026-06-09 21:36:22 +0700

    Update executable_media.sh to modify package installation
    Removed 'ufw' from the installation command and cleaned up the script.

--- dot_config/installSH/executable_media.sh
+++ dot_config/installSH/executable_media.sh
@@ -4,9 +4,7 @@ set -euo pipefail
 echo -e "${BLUE}-------> Install waybar and related modules ${NC}"
 
 sudo dnf copr enable -y erikreider/SwayNotificationCenter
-sudo dnf install -y waybar blueman pavucontrol zenity SwayNotificationCenter mpv mpv-mpris playerctl ufw
-
-flatpak install -y flathub org.localsend.localsend_app
+sudo dnf install -y waybar blueman pavucontrol zenity SwayNotificationCenter mpv mpv-mpris playerctl
 
 sudo firewall-cmd --add-port=53317/tcp --add-port=53317/udp --permanent
 sudo firewall-cmd --reload
