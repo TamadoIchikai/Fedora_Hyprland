7f89e240b5ef29cb2d7d6839d3ac92f03d5fe50f
TamadoIchikai <130322347+TamadoIchikai@users.noreply.github.com>
2026-06-09 21:33:17 +0700

    Update executable_waybar.sh

--- dot_config/installSH/executable_waybar.sh
+++ dot_config/installSH/executable_waybar.sh
@@ -7,7 +7,6 @@ set -e
 echo -e "${BLUE}-------> install waybar and some related modules ${NC}"
 sudo dnf copr enable -y erikreider/SwayNotificationCenter
 sudo dnf install -y waybar blueman pavucontrol zenity SwayNotificationCenter mpv mpv-mpris playerctl ufw
-flatpak install flathub org.localsend.localsend_app
 sudo firewall-cmd --add-port=53317/tcp --add-port=53317/udp --permanent
 sudo firewall-cmd --reload
 sudo firewall-cmd --list-ports
