8363e5d78c22d16510684c6f722fede38a4c13d3
TamadoIchikai <tamado.ichikai.kyooiku@gmail.com>
2026-06-11 01:34:17 +0700

    Fix theme issues

--- dot_config/installSH/executable_apps_extra.sh
+++ dot_config/installSH/executable_apps_extra.sh
@@ -17,8 +17,35 @@ echo -e "${GREEN}-------> DONE${NC}"
 echo -e "${BLUE}-------> Installing flatpak apps (OBS)${NC}"
 sudo dnf install -y flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install -y flathub com.obsproject.Studio
-flatpak install -y flathub io.github.mpc_qt.mpc-qt
-flatpak install -y flathub io.missioncenter.MissionCenter
 
+flatpak install -y flathub \
+  com.obsproject.Studio \
+  io.github.mpc_qt.mpc-qt \
+  io.missioncenter.MissionCenter
+
+echo -e "${BLUE}-------> Fix theme issues${NC}"
+sudo dnf install -y kvantum qt5ct qt6ct
+
+flatpak install -y flathub \
+  org.gtk.Gtk3theme.adw-gtk3 \
+  org.gtk.Gtk3theme.adw-gtk3-dark \
+  org.kde.KStyle.Kvantum
+
+mkdir -p ~/.config/Kvantum
+
+cat > ~/.config/Kvantum/kvantum.kvconfig <<'EOF'
+[General]
+theme=KvGnomeDark
+EOF
+
+flatpak override --user \
+  --env=GTK_THEME=adw-gtk3-dark \
+  --env=GTK_ICON_THEME=Papirus-Dark \
+  --env=ADW_DEBUG_COLOR_SCHEME=prefer-dark \
+  --env=QT_STYLE_OVERRIDE=kvantum \
+  --filesystem=xdg-config/Kvantum:ro \
+  --filesystem=xdg-config/gtk-3.0:ro \
+  --filesystem=xdg-config/gtk-4.0:ro \
+  --filesystem=xdg-data/icons:ro \
+  --filesystem=~/.icons:ro
 echo -e "${GREEN}-------> DONE${NC}"
