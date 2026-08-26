6287200bfd298909dfdeb1ed8befaf0a6a8e1e68
TamadoIchikai <tamado.ichikai.kyooiku@gmail.com>
2026-08-27 01:14:57 +0700

    Add backup tools
    rclone, restic, resticprofiles

--- dot_config/installSH/executable_apps_CLI.sh
+++ dot_config/installSH/executable_apps_CLI.sh
@@ -2,4 +2,5 @@
 set -euo pipefail
 
 sudo dnf copr enable -y dejan/lazygit
-sudo dnf install -y foot fuzzel fzf zoxide cliphist fuse fuse-libs qalculate setxkbmap trash-cli swappy btop lazygit duf gdu swayimg xournalpp lua-lgi nm-connection-editor wtype wofi ripgrep ImageMagick gawk wl-clipboard
+sudo dnf install -y foot restic rclone fuzzel fzf zoxide cliphist fuse fuse-libs qalculate setxkbmap trash-cli swappy btop lazygit duf gdu swayimg xournalpp lua-lgi nm-connection-editor wtype wofi ripgrep ImageMagick gawk wl-clipboard
+curl -sfL https://raw.githubusercontent.com/creativeprojects/resticprofile/master/install.sh | sh
