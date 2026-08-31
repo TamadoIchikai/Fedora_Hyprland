6402668a7ba912f2411d1db01d2e487b96b55825
TamadoIchikai <tamado.ichikai.kyooiku@gmail.com>
2026-08-31 19:03:33 +0700

    replace integrated compression/extraction tool with peazip and integrate into thunar right click menu

--- dot_config/installSH/executable_file_manager.sh
+++ dot_config/installSH/executable_file_manager.sh
@@ -5,7 +5,7 @@ sudo dnf install -y \
 thunar thunar-archive-plugin thunar-volman \
 gvfs gvfs-fuse udisks2 gvfs-smb gvfs-mtp \
 tumbler \
-xarchiver file-roller unzip p7zip p7zip-plugins unrar \
+peazip-qt6 unzip unrar 7zip \
 dconf gsettings-desktop-schemas \
 papirus-icon-theme adw-gtk3-theme \
 shared-mime-info xdg-utils desktop-file-utils \
